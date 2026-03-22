622e8cd0ed23b0800e384199d96814cc802ad740
Alexander Ng <alexng353@gmail.com>
2026-03-21 18:35:36 -0700

    feat: vertical scrolling, multiline rendering
    feat: cursor movement
    
    code is not perfect yet, no horizontal scrolling, no out of bounds
    checks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // use clap::Parser;
 use crossterm::{
     cursor,
-    event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
+    event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
     execute,
     terminal::{self, Clear, disable_raw_mode, enable_raw_mode},
 };
@@ -53,7 +53,7 @@ impl Buffer {
             dirty: false,
         }
     }
-    fn new_from_file(id: usize, filepath: &PathBuf) -> anyhow::Result<Self> {
+    fn new_from_file(id: usize, filepath: &PathBuf) -> Result<Self, std::io::Error> {
         let text = std::fs::read_to_string(filepath)?;
         Ok(Self {
             id,
@@ -65,27 +65,70 @@ impl Buffer {
 }
 
 /// Absolute position of the virtual cursor in the buffer
-struct Cursor {
+struct BufferCursor {
     line: usize,
     col: usize,
 }
-impl Cursor {
+impl BufferCursor {
     fn start() -> Self {
         Self { line: 0, col: 0 }
     }
 }
 
+struct ScreenCursor {
+    col: u16,
+    row: u16,
+}
+
 struct Window {
     buffer_id: usize,
-    cursor: Cursor,
+    cursor: BufferCursor,
     /// The absolute position of the first line that is visible
     scroll_offset: usize,
+}
+
+#[derive(PartialEq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Window {
+    fn move_cursor(&mut self, direction: Direction) {
+        match direction {
+            Direction::Up => {
+                self.cursor.line -= 1;
+            }
+            Direction::Down => {
+                self.cursor.line += 1;
+            }
+            Direction::Left => {
+                self.cursor.col -= 1;
+            }
+            Direction::Right => {
+                self.cursor.col += 1;
+            }
+        }
+    }
+    fn cursor_to_screen_coords(&self) -> ScreenCursor {
+        let row = self.cursor.line - self.scroll_offset;
+        let col = self.cursor.col;
+
+        // TODO: should be fine, we should probably panic if this is out of bounds
+        ScreenCursor {
+            col: col as u16,
+            row: row as u16,
+        }
+    }
 }
 
 struct Editor {
     buffers: Vec<Buffer>,
     current_window: Window,
     command_buffer: String,
+    mode: Mode,
 }
 
 impl Editor {
@@ -95,10 +138,11 @@ impl Editor {
             buffers: vec![buf0],
             current_window: Window {
                 buffer_id: 0,
-                cursor: Cursor::start(),
+                cursor: BufferCursor::start(),
                 scroll_offset: 0,
             },
             command_buffer: String::with_capacity(1024),
+            mode: Mode::Normal,
         }
     }
     fn max_id(&self) -> usize {
@@ -119,7 +163,7 @@ impl Editor {
 
         Ok(buf)
     }
-    fn open_file(&mut self, filepath: &PathBuf) -> anyhow::Result<usize> {
+    fn open_file(&mut self, filepath: &PathBuf) -> Result<usize, EditorError> {
         let id = self.max_id() + 1;
         self.buffers.push(Buffer::new_from_file(id, filepath)?);
         Ok(id)
@@ -128,15 +172,40 @@ impl Editor {
         // hypothetically impossible to error, but oh well
         &self.current_window
     }
+
+    // handles cursor movement within the window, scrolling, etc.
+    // window movement within the actual TUI is handled by the render_frame function
+    fn move_cursor(&mut self, direction: Direction) -> anyhow::Result<()> {
+        let current_offset = self.current_window.scroll_offset;
+        let current_line = self.current_window.cursor.line;
+        let current_col = self.current_window.cursor.col;
+
+        let (cols, rows) = terminal::size()?;
+
+        if direction == Direction::Up && current_line - current_offset == 0 {
+            self.current_window.scroll_offset -= 1;
+        } else if direction == Direction::Down
+            && ((current_line - current_offset) == (rows - 1).into())
+        {
+            self.current_window.scroll_offset += 1;
+        }
+
+        self.current_window.move_cursor(direction);
+
+        Ok(())
+    }
 }
 
 #[derive(Error, Debug)]
 enum EditorError {
     #[error("buffer {0} not found")]
     BufferNotFound(usize),
+
+    #[error("io error: {0}")]
+    IoError(#[from] std::io::Error),
 }
 
-// 1. calculate // 1. render the current buffer
+// TODO: rewrite this as a state machine that understands the window
 fn render_frame(stdout: &mut impl Write, editor: &Editor) -> anyhow::Result<()> {
     let (cols, rows) = terminal::size()?;
     clear_screen(stdout)?;
@@ -144,20 +213,45 @@ fn render_frame(stdout: &mut impl Write, editor: &Editor) -> anyhow::Result<()> 
 
     let current_window = editor.get_current_window();
 
-    for (line_number, line) in editor.buffers[current_window.buffer_id]
-        .text
-        .iter()
-        .enumerate()
-    {
-        print!("{:>3} ", line_number + 1);
-        print!("{}", line);
+    // command buffer line in the bottom
+    let num_lines = cols - 1;
+    let offset = current_window.scroll_offset;
+    let buffer = &editor.buffers[current_window.buffer_id];
+    let buffer_num_lines = buffer.text.len();
+
+    let start = offset;
+    let end = (offset + num_lines as usize).min(buffer_num_lines);
+
+    let lines = &buffer.text[start..end];
+
+    let mut row = 0;
+    for (line_number, line) in lines.iter().enumerate() {
+        execute!(stdout, cursor::MoveTo(0, row))?;
+        print!("{:>3} ", line_number + offset + 1);
+        let mut col = 4;
+        for ch in line.chars() {
+            if col >= cols {
+                break;
+            }
+            execute!(stdout, cursor::MoveTo(col, row))?;
+            write!(stdout, "{}", ch)?;
+            col += 1;
+        }
+        row += 1;
     }
 
-    execute!(stdout, cursor::MoveTo(0, rows - 1))?;
-    if !editor.command_buffer.is_empty() {
-        print!(":{}", editor.command_buffer);
-        stdout.flush()?;
+    match editor.mode {
+        Mode::Command => {
+            execute!(stdout, cursor::MoveTo(0, rows - 1))?;
+            print!(":{}", editor.command_buffer);
+            stdout.flush()?;
+        }
+        _ => {
+            let screen_cursor = current_window.cursor_to_screen_coords();
+            execute!(stdout, cursor::MoveTo(screen_cursor.col, screen_cursor.row))?;
+        }
     }
+
     Ok(())
 }
 
@@ -169,8 +263,6 @@ fn main() -> anyhow::Result<()> {
     execute!(stdout, terminal::EnterAlternateScreen)?;
 
     enable_raw_mode()?;
-
-    let mut mode = Mode::Normal;
 
     let mut editor = Editor::new();
 
@@ -188,16 +280,28 @@ fn main() -> anyhow::Result<()> {
                     println!("Exiting... (Ctrl+C)");
                     break;
                 }
-                match mode {
+                match editor.mode {
                     Mode::Normal => match code {
                         KeyCode::Char('q') => {
                             break;
+                        }
+                        KeyCode::Char('h') => {
+                            editor.move_cursor(Direction::Left)?;
+                        }
+                        KeyCode::Char('j') => {
+                            editor.move_cursor(Direction::Down)?;
+                        }
+                        KeyCode::Char('k') => {
+                            editor.move_cursor(Direction::Up)?;
+                        }
+                        KeyCode::Char('l') => {
+                            editor.move_cursor(Direction::Right)?;
                         }
                         // KeyCode::Char('i') => {
                         //     mode = Mode::Insert;
                         // }
                         KeyCode::Char(':') => {
-                            mode = Mode::Command;
+                            editor.mode = Mode::Command;
                         }
                         _ => {
                             // // for debug
@@ -206,13 +310,13 @@ fn main() -> anyhow::Result<()> {
                         }
                     },
                     Mode::Insert => match code {
-                        KeyCode::Char('i') => mode = Mode::Insert,
-                        KeyCode::Char('q') => mode = Mode::Normal,
+                        KeyCode::Char('i') => editor.mode = Mode::Insert,
+                        KeyCode::Char('q') => editor.mode = Mode::Normal,
                         _ => {}
                     },
                     Mode::Command => match code {
                         KeyCode::Esc => {
-                            mode = Mode::Normal;
+                            editor.mode = Mode::Normal;
                         }
                         KeyCode::Enter => {
                             if editor.command_buffer == "q" {
@@ -225,10 +329,14 @@ fn main() -> anyhow::Result<()> {
                                 editor.current_window.buffer_id = id;
                             }
                             editor.command_buffer.clear();
-                            mode = Mode::Normal;
+                            editor.mode = Mode::Normal;
                         }
                         KeyCode::Backspace => {
-                            editor.command_buffer.pop();
+                            if editor.command_buffer.is_empty() {
+                                editor.mode = Mode::Normal;
+                            } else {
+                                editor.command_buffer.pop();
+                            }
                         }
                         KeyCode::Char(c) => {
                             editor.command_buffer.push(c);
@@ -242,6 +350,7 @@ fn main() -> anyhow::Result<()> {
     }
 
     disable_raw_mode()?;
+    println!("Bye!");
     Ok(())
 }
 
